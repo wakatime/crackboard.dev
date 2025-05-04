ee4a7e4444e55c6a7e20644df0416043e5d26d43
Alan Hamlett <alan.hamlett@gmail.com>
2025-05-04 08:55:03 +0200

    do get or create inside db transaction

--- packages/core/src/backend/helpers/leaderboard.ts
+++ packages/core/src/backend/helpers/leaderboard.ts
@@ -6,15 +6,21 @@ import type { UpdateLeaderboardConfigData } from '../../validators';
 const LEADERBOARD_CONFIG_ID = 'leaderboard-config';
 
 export const getLeaderboardConfig = async () => {
-  const [config] = await db.select().from(LeaderboardConfig).where(eq(LeaderboardConfig.id, LEADERBOARD_CONFIG_ID));
-  if (config) {
-    return config;
-  }
-  const [newConfig] = await db.insert(LeaderboardConfig).values({ id: LEADERBOARD_CONFIG_ID }).returning();
-  if (!newConfig) {
+  const config = await db.transaction(async (tx) => {
+    const [config] = await tx.select().from(LeaderboardConfig).where(eq(LeaderboardConfig.id, LEADERBOARD_CONFIG_ID));
+    if (config) {
+      return config;
+    }
+    const [newConfig] = await tx.insert(LeaderboardConfig).values({ id: LEADERBOARD_CONFIG_ID }).returning();
+    if (!newConfig) {
+      return (await tx.select().from(LeaderboardConfig).where(eq(LeaderboardConfig.id, LEADERBOARD_CONFIG_ID)))[0];
+    }
+    return newConfig;
+  });
+  if (!config) {
     throw new Error('Failed to get config!');
   }
-  return newConfig;
+  return config;
 };
 
 export const updateLeaderboardConfig = async (options: UpdateLeaderboardConfigData) => {
