0a06b9d792d61265d720124a6c7393a52f1d55cd
Alan Hamlett <alan.hamlett@gmail.com>
2025-05-05 10:16:44 +0200

    chunkify does nothing to speed up loop

--- packages/tasks/src/summaries/syncSummariesForAllUsers.ts
+++ packages/tasks/src/summaries/syncSummariesForAllUsers.ts
@@ -2,16 +2,12 @@ import { db } from '@workspace/db/drizzle';
 import { User } from '@workspace/db/schema';
 
 import { wakaq } from '..';
-import { chunkify } from '../utils/helpers';
 import { syncUserSummaries } from './syncUserSummaries';
 
 export const syncSummariesForAllUsers = wakaq.task(
   async () => {
     const allUsers = await db.select({ id: User.id }).from(User);
-    const chunks = chunkify(allUsers, 20);
-    for (const chunk of chunks) {
-      await Promise.all(chunk.map((user) => syncUserSummaries.enqueue(user.id)));
-    }
+    await Promise.all(allUsers.map((user) => syncUserSummaries.enqueue(user.id)));
   },
   { name: 'getSummaryForAllUsers' },
 );
