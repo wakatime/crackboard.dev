cf5298ad9516956ead3f83e8e5abf97f339d002e
Alan Hamlett <alan.hamlett@gmail.com>
2025-05-05 14:02:45 +0200

    import tasks so treeshaking doesnt remove them from scheduler script

--- packages/tasks/src/scripts/wakaqChild.ts
+++ packages/tasks/src/scripts/wakaqChild.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import 'dotenv/config';
 
-import * as syncSummariesForAllUsers from '@workspace/tasks/summaries/syncSummariesForAllUsers';
 import { WakaQChildWorker } from 'wakaq';
 
 import { wakaq } from '..';
 import * as registerWithDirectory from '../register/registerWithDirectory';
+import * as syncSummariesForAllUsers from '../summaries/syncSummariesForAllUsers';
 import * as syncUserSummaries from '../summaries/syncUserSummaries';
 
 await new WakaQChildWorker(wakaq).start();
--- packages/tasks/src/scripts/wakaqScheduler.ts
+++ packages/tasks/src/scripts/wakaqScheduler.ts
@@ -1,8 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import 'dotenv/config';
 
 import { WakaQScheduler } from 'wakaq';
 
 import { wakaq } from '..';
+import * as registerWithDirectory from '../register/registerWithDirectory';
+import * as syncSummariesForAllUsers from '../summaries/syncSummariesForAllUsers';
+import * as syncUserSummaries from '../summaries/syncUserSummaries';
 
 const scheduler = new WakaQScheduler(wakaq);
 await scheduler.start();
