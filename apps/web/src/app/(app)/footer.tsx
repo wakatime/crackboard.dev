756fb08e24264c427e4b6d73d906dc6fa4584919
Alan Hamlett <alan.hamlett@gmail.com>
2025-05-05 14:18:03 +0200

    link to prod version on github

--- apps/web/src/app/(app)/footer.tsx
+++ apps/web/src/app/(app)/footer.tsx
@@ -35,12 +35,28 @@ export default function Footer() {
   return (
     <footer className="bg-background mt-20">
       <div className="container mx-auto flex h-28 items-center gap-4 px-4 md:px-12">
-        <div className="text-muted-foreground ml-2 flex-1 text-sm">{text}</div>
+        <div className="ml-2 flex-1">
+          <p className="text-muted-foreground text-sm">{text}</p>
+        </div>
 
         <div className="mr-2 flex items-center gap-2">
-          <Link href="https://github.com/wakatime/crackboard.dev" title={leaderboardConfigQuery.data.commitSha}>
-            <FaGithub />
-          </Link>
+          <p className="text-muted-foreground text-sm">
+            <Link
+              href="https://github.com/wakatime/crackboard.dev"
+              title={leaderboardConfigQuery.data.commitSha}
+              className="text-muted-foreground"
+            >
+              <FaGithub className="inline" />
+            </Link>
+            {' · '}
+            <Link
+              href={`https://github.com/wakatime/crackboard.dev/commit/${leaderboardConfigQuery.data.commitSha}`}
+              title="Current production version"
+              className="text-muted-foreground"
+            >
+              {leaderboardConfigQuery.data.commitSha}
+            </Link>
+          </p>
         </div>
       </div>
     </footer>
